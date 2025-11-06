76830bb10c416cfb7ff4dd2da3f04691295827f8
sukra <sukra24si@mahasiswa.pcr.ac.id>
2025-11-06 18:40:30 -0300

    Soal 1

---
+++ resources/views/layouts/guest/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>HairCut - Hair Salon HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    @include('layouts.guest')
+
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    @include('layouts.guest.navbar')
+
+
+    @yield('content')
+
+
+    @include('layouts.guest.footer')
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    @include('layouts.guest.js')
+</body>
+
+</html>
